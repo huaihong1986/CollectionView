72a95abd90d105f09ed8dc2bcfea8ce5d35db5ac
huaihong1986 <huaihong1986@sina.com>
2015-08-20 10:52:18 +0800

    CollectionViewDemo

--- CollectionView/ViewController.h
+++ CollectionView/ViewController.h
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController<UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>{
+    
+}
+@property (strong, nonatomic)UICollectionView *collectionView;
 
 @end
 
