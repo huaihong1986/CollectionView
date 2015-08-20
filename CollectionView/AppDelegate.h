4d1c4cb434fe3d8e2767f67e4371626093cf3f2e
huaihong1986 <huaihong1986@sina.com>
2015-08-20 09:43:09 +0800

    Initial Commit

---
+++ CollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionView
+//
+//  Created by Hu Aihong on 15-8-20.
+//  Copyright (c) 2015年 ChinaCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
